9c7b7620fbc88326e40230cdb95428f3644fbeab
Phong <ngtuanphong98@gmail.com>
2025-07-16 04:11:40 +1000

    combine fe and be

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,8 @@
-// Load dependencies
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
-// Import routes
 const productRoutes = require('./routes/productRoutes.js');
 const orderRoutes = require('./routes/orderRoutes.js');
 const discountRoutes = require('./routes/discountRoutes.js');
@@ -14,15 +13,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Root route
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
+// API Routes
+app.get('/api', (req, res) => {
+  res.send('Backend API is running!');
 });
 
-// Register routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 app.use('/discount', discountRoutes);
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all route to serve index.html for React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // Start server
 const PORT = process.env.PORT || 3000;
